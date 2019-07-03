95a8acf4df45a64efcb0064b0a5e02a3bb6fa7ab
morrisonmburu <morrisonmburu7@gmail.com>
2019-07-03 12:10:51 +0300

    send push

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -214,6 +214,47 @@ class orderController extends Controller
         ];
 
         // $this->push();
+        //push notification
+        $content = array(
+            "en" => 'English Message'
+            );
+        
+        $fields = array(
+            'app_id' => "e4cda120-72b2-433a-9b6b-be1e8ef48024",
+            'included_segments' => array('Active Users'),
+            'data' => array("foo" => "bar"),
+            'contents' => $content
+        );
+        
+        $fields = json_encode($fields);
+        print("\nJSON sent:\n");
+        print($fields);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                                   'Authorization: Basic YOUR_REST_API_KEY'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return $response;
+    }
+    
+    $response = sendMessage();
+    $return["allresponses"] = $response;
+    $return = json_encode( $return);
+    
+    print("\n\nJSON received:\n");
+    print($return);
+    print("\n");
+
+    //top push nofification
   
         Notification::send($user, new adminNotification($details));
 
