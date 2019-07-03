086834c95f5c4d486c344e5c9b70c56582a3d3a7
morrisonmburu <morrisonmburu7@gmail.com>
2019-07-03 12:25:10 +0300

    fixing bug

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -213,48 +213,14 @@ class orderController extends Controller
             'order_id' => $data['id']
         ];
 
-        // $this->push();
         //push notification
-        $content = array(
-            "en" => 'English Message'
-            );
+        $response = $this->sendMessage();
+        $return["allresponses"] = $response;
+        $return = json_encode( $return);
         
-        $fields = array(
-            'app_id' => "e4cda120-72b2-433a-9b6b-be1e8ef48024",
-            'included_segments' => array('Active Users'),
-            'data' => array("foo" => "bar"),
-            'contents' => $content
-        );
-        
-        $fields = json_encode($fields);
-        print("\nJSON sent:\n");
-        print($fields);
-        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
-                                                   'Authorization: Basic YOUR_REST_API_KEY'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, FALSE);
-        curl_setopt($ch, CURLOPT_POST, TRUE);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-        
-        return $response;
-    }
-    
-    $response = sendMessage();
-    $return["allresponses"] = $response;
-    $return = json_encode( $return);
-    
-    print("\n\nJSON received:\n");
-    print($return);
-    print("\n");
-
-    //top push nofification
+        print("\n\nJSON received:\n");
+        print($return);
+        print("\n");
   
         Notification::send($user, new adminNotification($details));
 
@@ -317,6 +283,38 @@ class orderController extends Controller
         session::flash('success', 'The order was successfully edited!');
 
         return redirect()->route('orders.index');
+    }
+
+     public function sendMessage(){
+        $content = array(
+            "en" => 'English Message'
+            );
+        
+        $fields = array(
+            'app_id' => "5eb5a37e-b458-11e3-ac11-000c2940e62c",
+            'included_segments' => array('Active Users'),
+            'data' => array("foo" => "bar"),
+            'contents' => $content
+        );
+        
+        $fields = json_encode($fields);
+        print("\nJSON sent:\n");
+        print($fields);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charset=utf-8',
+                                                   'Authorization: Basic YOUR_REST_API_KEY'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return $response;
     }
 
     public function deleteorder2(Request $request){
