d6207ea68e56d3ed54d722bad5d7fc6133df6f0f
morrisonmburu <morrisonmburu7@gmail.com>
2019-07-03 15:37:54 +0300

    fixing bugs

--- app/Http/Controllers/Auth/adminRegisterController.php
+++ app/Http/Controllers/Auth/adminRegisterController.php
@@ -38,7 +38,7 @@ class adminRegisterController extends Controller
 
       $this->validate($request, array(
         'name' => ['required', 'string', 'max:255'],
-        'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+        'email' => ['required', 'string', 'email', 'max:255'],
         'password' => ['required', 'string', 'min:8', 'confirmed'],
       ));
 
