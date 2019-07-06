120fa7f4489a4215100cb67b4aff110d3fb8f55b
morrisonmburu <morrisonmburu7@gmail.com>
2019-07-06 21:22:01 +0300

    Adding some effects

--- resources/views/backend/mails.blade.php
+++ resources/views/backend/mails.blade.php
@@ -4,7 +4,7 @@ Hi <b>{{$name}}</b>,
 
 {{ $edit_info }}
 
-{{ $cancelinfo }}
+{{-- {{ $cancelinfo }} --}}
 
 <style type="text/css">
 .card{position:relative;display:-ms-flexbox;display:flex;-ms-flex-direction:column;flex-direction:column;min-width:0;word-wrap:break-word;background-color:#fff;background-clip:border-box;border:1px solid rgba(0,0,0,.125);border-radius:.25rem}.card>hr{margin-right:0;margin-left:0}
