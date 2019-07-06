fa9c966155552ab4fa70af0f63828c3b824535fc
morrisonmburu <morrisonmburu7@gmail.com>
2019-07-06 20:36:43 +0300

    Adding some effects

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -51,11 +51,11 @@
                             <li class="nav-item">
                                 <a class="nav-link" style="color: #fff;" href="{{ route('admins') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                           {{--  @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" style="color: #fff;" href="{{ route('register_admin') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            @endif --}}
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
